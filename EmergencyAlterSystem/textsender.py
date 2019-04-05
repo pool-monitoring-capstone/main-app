2283dff02711fda583c4d0a1cfe04fbd13c939d9
akhalid7 <46307349+akhalid7@users.noreply.github.com>
2019-04-05 10:07:09 -0400

    Update textsender.py

--- EmergencyAlterSystem/textsender.py
+++ EmergencyAlterSystem/textsender.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 
-account_sid = 'AC3271de3b0b95b18d00ca1febab4db43f'
-auth_token = '5c899e83042f96227a7b112d163b88a7'
+account_sid = ''
+auth_token = ''
 client = Client(account_sid, auth_token)
 
 def send_sms(phoneNumner, body):
